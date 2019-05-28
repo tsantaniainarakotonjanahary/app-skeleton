7b87f858433e50cd673212a666d6da8c3009b4c4
Terje Uglebakken <teug91@gmail.com>
2019-05-29 00:22:43 +0200

    chore: initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '..'
+
+export const App = () => (
+    <>
+        <Header/>
+        <p>Hello world!</p>
+    </>
+)
+
+
+
---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { DataProvider } from '@dhis2/app-runtime'
+import { HeaderBar } from '@dhis2/ui-widgets'
+
+export const Header = () => (
+    <p>HI</p>
+)
+
