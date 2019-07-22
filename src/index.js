f484dfb8dc6d1da51c03ef44d4c23c2a530128df
Terje Uglebakken <teug91@gmail.com>
2019-07-23 00:19:56 +0200

    docs: add comments for variables

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import { App } from './components/App'
 
+/**
+ * This is what is displayed in the header bar
+ */
 const appName = 'App Name'
+
+/**
+ * Passed to DataProvider to get various data for the header bar.
+ * 
+ * If there is another minor release to for example 2.32.2,
+ * then this variable must be updated.
+ * It's possible to ommit the minor version number,
+ * i.e. http://play.dhis2.org/2.32,
+ * though it might lead to CORS errors during development.
+ */
 const developmentServer = 'https://play.dhis2.org/2.32.1'
+
+/**
+ * Passed to DataProvider to get various data for the header bar.
+ */
 const apiVersion = 32
+
 const rootElement = document.getElementById('root')
 
 const withBaseUrl = baseUrl => {
