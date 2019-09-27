7d6b6a0838900968d1cbad3a815c8a1c607b7aa0
Terje Uglebakken <teug91@gmail.com>
2019-09-27 13:46:52 +0200

    feat: add query

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { CssReset } from '@dhis2/ui-core'
-import { DataProvider } from '@dhis2/app-runtime'
+import { Provider } from '@dhis2/app-runtime'
 import { HeaderBar } from '@dhis2/ui-widgets'
 import { Main } from '../Main'
 import 'typeface-roboto'
 import './style.css'
 
-const baseUrl = process.env.REACT_APP_DHIS2_BASE_URL
-const apiVersion = process.env.REACT_APP_DHIS2_API_VERSION
+const config = {
+    baseUrl: process.env.REACT_APP_DHIS2_BASE_URL,
+    apiVersion: process.env.REACT_APP_DHIS2_API_VERSION,
+}
 
 export const App = ({ appName }) => (
-    <>
+    <Provider config={config}>
         <CssReset />
-        <DataProvider baseUrl={baseUrl} apiVersion={apiVersion}>
-            <HeaderBar appName={appName} className="headerbar" />
-            <Main />
-        </DataProvider>
-    </>
+        <HeaderBar appName={appName} />
+        <Main />
+    </Provider>
 )
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import { Card } from '@dhis2/ui-core'
+import { useDataQuery } from '@dhis2/app-runtime'
+import { Card, ScreenCover, CircularLoader } from '@dhis2/ui-core'
 import './style.css'
 
-export const Main = () => (
-    <main id="main">
-        <Card className="padded-card">
-            <h1>Hello world</h1>
-        </Card>
-    </main>
-)
+const query = {
+    me: {
+        resource: 'me',
+        params: {
+            fields: 'displayName',
+        },
+    },
+}
+
+export const Main = () => {
+    const { loading, error, data } = useDataQuery(query)
+
+    if (loading)
+        return (
+            <ScreenCover>
+                <CircularLoader />
+            </ScreenCover>
+        )
+
+    if (error) return <span>{error.message}</span>
+
+    return (
+        <main id="main">
+            <Card className="padded-card">
+                <h1>Hello {data.me.displayName}</h1>
+            </Card>
+        </main>
+    )
+}
